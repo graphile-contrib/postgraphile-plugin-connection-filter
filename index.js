9a94c9cb5d1d3549255fb8de4eb3bc6c725d721f
Matt Bretl <mpbretl@gmail.com>
2018-09-12 16:48:01 +0100

    Refactor computed columns support

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 module.exports = function PostGraphileConnectionFilterPlugin(builder, options) {
+  const { connectionFilterComputedColumns = true } = options;
+
   require("./src/ConnectionArgFilterPlugin.js")(builder, options);
   require("./src/PgConnectionArgFilterPlugin.js")(builder, options);
   require("./src/PgConnectionArgFilterColumnsPlugin.js")(builder, options);
-  require("./src/PgConnectionArgFilterComputedColumnsPlugin.js")(
-    builder,
-    options
-  );
+  if (connectionFilterComputedColumns) {
+    require("./src/PgConnectionArgFilterComputedColumnsPlugin.js")(
+      builder,
+      options
+    );
+  }
   require("./src/PgConnectionArgFilterOperatorsPlugin.js")(builder, options);
 };
