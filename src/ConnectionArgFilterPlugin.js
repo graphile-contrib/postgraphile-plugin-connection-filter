36b7f14889ae08b0ac9f7bb55ffaa50b0740a37c
Matt Bretl <mpbretl@gmail.com>
2018-09-12 15:51:37 +0100

    Use inflector instead of template literals

--- src/ConnectionArgFilterPlugin.js
+++ src/ConnectionArgFilterPlugin.js
@@ -54,4 +54,18 @@ module.exports = function ConnectionArgFilterPlugin(
       connectionFilterAllowedFieldTypes,
     });
   });
+
+  builder.hook("inflection", inflection => {
+    return Object.assign(inflection, {
+      filterType(typeName) {
+        return `${typeName}Filter`;
+      },
+      filterFieldType(typeName) {
+        return `${typeName}Filter`;
+      },
+      filterFieldListType(typeName) {
+        return `${typeName}ListFilter`;
+      },
+    });
+  });
 };
