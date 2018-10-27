602d1b6214c25452e4f6138afa498e88748f0b8f
Matt Bretl <mpbretl@gmail.com>
2018-10-27 18:31:13 +0100

    Add test for computed column returning scalar array

--- __tests__/p-schema.sql
+++ __tests__/p-schema.sql
@@ -60,6 +60,17 @@ $$ language sql stable;
 
 comment on function p.filterable_computed2(p.filterable) is E'@omit filter';
 
+create function p.filterable_computed_int_array(f p.filterable) returns int[] as $$
+  select
+    case
+      when f.id = 1 then array[1, 10]
+      when f.id = 2 then array[2, 20]
+      when f.id = 3 then array[3, 30]
+      when f.id = 4 then array[4, 40]
+      else null
+    end;
+$$ language sql stable;
+
 create function p.filterable_computed_setof_child(f p.filterable) returns setof p.child as $$
   select p.child.*
   from p.child
