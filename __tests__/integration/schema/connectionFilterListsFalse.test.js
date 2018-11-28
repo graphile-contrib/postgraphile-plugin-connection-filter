51474bf99576b1a1bd0752412fa9d11de21bd6a2
Matt Bretl <mpbretl@gmail.com>
2018-11-28 16:17:15 +0000

    Tweak naming/wording of option tests

--- __tests__/integration/schema/connectionFilterListsFalse.test.js
+++ __tests__/integration/schema/connectionFilterListsFalse.test.js
@@ -2,14 +2,14 @@ const core = require("./core");
 const { PgConnectionArgCondition } = require("graphile-build-pg");
 
 test(
-  "prints a schema with the filter plugin and the connectionFilterRelations option",
+  "prints a schema with the filter plugin and the `connectionFilterLists: false` option",
   core.test(["p"], {
     skipPlugins: [PgConnectionArgCondition],
     appendPlugins: [require("../../../index.js")],
     disableDefaultMutations: true,
     legacyRelations: "omit",
     graphileBuildOptions: {
-      connectionFilterRelations: true,
+      connectionFilterLists: false,
     },
   })
 );
