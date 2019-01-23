8e73a4f9bcde96fcfbfaf48471a6736a1327c536
Matt Bretl <mpbretl@gmail.com>
2019-01-23 00:17:34 +0000

    chore: add test for connectionFilterAllowedFieldTypes option

---
+++ __tests__/integration/schema/connectionFilterAllowedFieldTypes.test.js
@@ -0,0 +1,15 @@
+const core = require("./core");
+const { PgConnectionArgCondition } = require("graphile-build-pg");
+
+test(
+  "prints a schema with the filter plugin and the connectionFilterAllowedFieldTypes option",
+  core.test(["p"], {
+    skipPlugins: [PgConnectionArgCondition],
+    appendPlugins: [require("../../../index.js")],
+    disableDefaultMutations: true,
+    legacyRelations: "omit",
+    graphileBuildOptions: {
+      connectionFilterAllowedFieldTypes: ["String", "Int"],
+    },
+  })
+);
