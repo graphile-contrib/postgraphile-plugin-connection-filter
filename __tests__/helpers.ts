8d71011397324ab7a6cbcf1fe96781125894a541
Matt Bretl <mattbretl@users.noreply.github.com>
2022-12-01 08:41:50 -0800

    chore(deps): update `graphql` version used in tests (#188)
    - Update `graphql` to `15.8.0`
    - Replace custom schema sorting helper with `lexicographicSortSchema` from `graphql/utilities`
    - Update test snapshots to match the order produced by `lexicographicSortSchema`

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,6 +1,6 @@
 import * as pg from "pg";
 import { parse, buildASTSchema, GraphQLSchema } from "graphql";
-import { printSchema } from "graphql/utilities";
+import { lexicographicSortSchema, printSchema } from "graphql/utilities";
 
 export async function withPgPool<T>(
   cb: (pool: pg.Pool) => Promise<T>
@@ -46,33 +46,5 @@ export function printSchemaOrdered(originalSchema: GraphQLSchema): string {
   // Clone schema so we don't damage anything
   const schema = buildASTSchema(parse(printSchema(originalSchema)));
 
-  const typeMap = schema.getTypeMap();
-  Object.keys(typeMap).forEach((name) => {
-    const gqlType = typeMap[name];
-
-    // Object?
-    if ("getFields" in gqlType) {
-      const fields = gqlType.getFields();
-      const keys = Object.keys(fields).sort();
-      keys.forEach((key) => {
-        const value = fields[key];
-
-        // Move the key to the end of the object
-        delete fields[key];
-        fields[key] = value;
-
-        // Sort args
-        if ("args" in value) {
-          value.args.sort((a, b) => a.name.localeCompare(b.name));
-        }
-      });
-    }
-
-    // Enum?
-    if ("getValues" in gqlType) {
-      gqlType.getValues().sort((a, b) => a.name.localeCompare(b.name));
-    }
-  });
-
-  return printSchema(schema);
+  return printSchema(lexicographicSortSchema(schema));
 }
