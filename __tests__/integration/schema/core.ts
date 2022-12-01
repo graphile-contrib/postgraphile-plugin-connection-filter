00c43b207bfdf057cf1b8c118ae90ca1dc4bdaec
Matt Bretl <mpbretl@gmail.com>
2022-11-30 23:14:31 -0800

    run prettier:fix

--- __tests__/integration/schema/core.ts
+++ __tests__/integration/schema/core.ts
@@ -2,19 +2,21 @@ import * as pg from "pg";
 import { createPostGraphileSchema } from "postgraphile-core";
 import { printSchemaOrdered, withPgClient } from "../../helpers";
 
-export const test = (
-  schemas: string[],
-  options: Record<string, unknown>,
-  setup?: (client: pg.PoolClient) => void
-) => (): Promise<void> =>
-  withPgClient(async (client) => {
-    if (setup) {
-      if (typeof setup === "function") {
-        await setup(client);
-      } else {
-        await client.query(setup);
+export const test =
+  (
+    schemas: string[],
+    options: Record<string, unknown>,
+    setup?: (client: pg.PoolClient) => void
+  ) =>
+  (): Promise<void> =>
+    withPgClient(async (client) => {
+      if (setup) {
+        if (typeof setup === "function") {
+          await setup(client);
+        } else {
+          await client.query(setup);
+        }
       }
-    }
-    const schema = await createPostGraphileSchema(client, schemas, options);
-    expect(printSchemaOrdered(schema)).toMatchSnapshot();
-  });
+      const schema = await createPostGraphileSchema(client, schemas, options);
+      expect(printSchemaOrdered(schema)).toMatchSnapshot();
+    });
