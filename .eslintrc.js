0f7112f5e1c06e42382486c095d10219e0f09901
Benjie Gillam <benjie@jemjie.com>
2023-11-16 08:41:15 +0000

    Sync eslint config with crystal repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,35 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    sourceType: "module",
+  },
   env: {
+    jest: true,
     node: true,
     es6: true,
     "jest/globals": true,
   },
-  plugins: ["@typescript-eslint", "jest", "graphile-export"],
+  plugins: [
+    "@typescript-eslint",
+    "jest",
+    //"tsdoc",
+    //"simple-import-sort",
+    //"import",
+    "graphile-export",
+  ],
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    // "plugin:import/errors",
+    // "plugin:import/typescript",
     "plugin:graphile-export/recommended",
     "plugin:jest/recommended",
     "prettier",
   ],
   rules: {
     "jest/expect-expect": ["off"],
+    "@typescript-eslint/no-var-requires": ["off"],
   },
 };
